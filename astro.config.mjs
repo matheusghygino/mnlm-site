29eed5849d5fe58ddb2343ed73ec8eb5d3d625c9
matheusghyino <matheusghygino@gmail.com>
2026-02-06 03:46:48 -0300

    voltando para estatico

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node ({
-    mode: 'standalone',
-  }),
+  output: "static",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [preact()],
 });
